9081f7aa743630077ece642f8601f58754c0d50f
Vilya22 <roma.vilchynskyy@gmail.com>
2020-06-24 15:31:14 +0300

    cart counter functional updated, like functional added

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-quantityOfProducts = 1
+let cartCounter = document.getElementById("cart_counter");
+let addToCartButtons = document.querySelectorAll(".add_to_cart_btn");
 
-function addToCart () {
-    let cartCounter = document.querySelector(".cart_counter").innerHTML = quantityOfProducts
-    quantityOfProducts++
-    return quantityOfProducts
+for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", function() {
+        cartCounter.textContent = +cartCounter.textContent + 1;      
+    })
+}
+
+
+let likeButtons = document.querySelectorAll(".like");
+for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener("click", function () {
+        if (likeButtons[i].classList.contains("liked")) {
+            likeButtons[i].classList.remove("liked")
+        } else {
+            likeButtons[i].classList.add("liked")
+        }
+    })
 }
