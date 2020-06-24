f88ec2f454809e12919494da997a79daea0507b7
Vilya22 <roma.vilchynskyy@gmail.com>
2020-06-24 21:13:59 +0300

    modal added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// add to cart
+
 let cartCounter = document.getElementById("cart_counter");
 let addToCartButtons = document.querySelectorAll(".add_to_cart_btn");
 
@@ -7,14 +9,37 @@ for (let i = 0; i < addToCartButtons.length; i++) {
     })
 }
 
+// change like button state
 
 let likeButtons = document.querySelectorAll(".like");
 for (let i = 0; i < likeButtons.length; i++) {
     likeButtons[i].addEventListener("click", function () {
-        if (likeButtons[i].classList.contains("liked")) {
-            likeButtons[i].classList.remove("liked")
-        } else {
-            likeButtons[i].classList.add("liked")
-        }
+        this.classList.toggle("liked");
     })
 }
+
+// modal
+
+let moreDetailsButtons = document.querySelectorAll(".detail_btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".btn_close")
+
+moreDetailsButtons.forEach(function(btn) {
+    btn.addEventListener("click", function () {
+        modal.classList.add("show")
+        modal.classList.remove("hide")
+    })
+})
+
+function closeModal() {
+    modal.classList.add("hide")
+    modal.classList.remove("show")
+}
+
+closeBtn.addEventListener("click", closeModal)
+
+modal.addEventListener("click", function(e) {
+    if (e.target === modal) {
+        closeModal()
+    }
+})
